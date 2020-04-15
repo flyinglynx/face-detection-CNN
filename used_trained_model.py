584fbf2c3abf4a6475c46d0711c0784b9f18ad75
Flying Lynx <u201714430@hust.edu.cn>
2020-04-15 22:37:12 +0800

    add a example of using trained model

---
+++ used_trained_model.py
@@ -0,0 +1,23 @@
+import torch
+import cv2
+import numpy as np
+
+
+#检查cuda是否可用
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+detectModel = torch.load("trained_model/CNNmodel.pt")
+model = detectModel.to(device)   #如果cuda可用，将模型改为GPU运算
+
+testImg = cv2.imread("test.jpg",0)
+testImg = cv2.resize(testImg,(100,100),interpolation = cv2.INTER_AREA)
+testImg = testImg/255.0
+testImg = testImg.astype(np.float32)
+
+inputTensor = torch.from_numpy(testImg)
+inputTensor = inputTensor.to(device)
+
+inputTensor= inputTensor.view(1,1,100,100)
+
+print(model(inputTensor))
+
